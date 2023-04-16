87af0494840916dde97c30d988194fa3cf9cc1dd
ThalesDFerreira <thalesferreira190@gmail.com>
2023-04-16 16:22:09 -0300

    Implementando o Vue Router

---
+++ frontend-vue/src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="nav">
+    <router-link to="/">
+      <img src="" alt="logo" id="logo">
+    </router-link>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/pedidos">Pedidos</router-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+};
+</script>
+
+<style scoped>
+</style>
