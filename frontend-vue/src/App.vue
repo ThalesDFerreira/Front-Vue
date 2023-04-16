1bcef91998c1f2299a70ecd0cbdfdce192fe590c
ThalesDFerreira <thalesferreira190@gmail.com>
2023-04-16 16:56:37 -0300

    Finalizando cabeçado e rodapé do projeto1~

--- frontend-vue/src/App.vue
+++ frontend-vue/src/App.vue
@@ -1,20 +1,43 @@
 <template>
-  <Navbar/>
-  <router-view/>
-  <Footer/>
+  <div>
+    <Navbar :logo="logo_src" :alt="app_name" />
+    <div><router-view /></div>
+    <Footer />
+  </div>
 </template>
 
 <script>
-import Navbar from './components/Navbar.vue';
-import Footer from './components/Footer.vue';
-
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 export default {
+  data: function () {
+    return {
+      logo_src: '/img/logo.png',
+      app_name: 'Burger',
+    };
+  },
   components: {
     Navbar,
-    Footer
-  }
-}
+    Footer,
+  },
+};
 </script>
 
 <style>
+* {
+  font-family: Helvetica;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+.main-container {
+  margin: 50px;
+  min-height: 250px;
+}
+h1 {
+  text-align: center;
+  font-size: 42px;
+  margin-bottom: 30px;
+  color: #222;
+}
 </style>
--- frontend-vue/src/views/Home.vue
+++ frontend-vue/src/views/Home.vue
@@ -1,9 +1,7 @@
 <template>
-  <div class="home">
+  <div class="main-container">
     <h1>HOME</h1>
   </div>
 </template>
 
-<script>
-
-</script>
+<script></script>
